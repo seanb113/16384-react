896204fe9dd41bf4ab7cc1e622315c6eccd40ec2
Sean Beach <sean.beach1@yahoo.com>
2021-05-07 23:42:34 -0400

    m

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -20,7 +20,6 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
         navigator.serviceWorker.ready.then(() => {
